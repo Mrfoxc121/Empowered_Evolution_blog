25a1db9a8f84fcf4682b499854f8de4135556136
Mrfoxc121 <cfox0115@gmail.com>
2024-03-25 13:19:07 +0000

    changed metadata 25/3/24

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -7,7 +7,7 @@ const siteMetadata = {
     theme: 'system', // system, dark or light
     siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
     siteLogo: '/logo.png',
-    socialBanner: '/social-banner.png', // add social banner in the public folder
+    socialBanner: '/SocialBanner.png', // add social banner in the public folder
     email: 'foxc121@gmail.com', 
     github: 'https://github.com',
     twitter: 'https://twitter.com/',
